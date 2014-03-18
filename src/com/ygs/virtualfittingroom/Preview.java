49b3d6e5fc229824e50c9565314295a7ce56787d
sankar <ygsankar.87@gmail.com>
2014-03-18 13:17:16 +0530

    Update Preview.java
    Camera orientation changes updated

--- src/com/ygs/virtualfittingroom/Preview.java
+++ src/com/ygs/virtualfittingroom/Preview.java
@@ -14,6 +14,8 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.view.Surface;
+
 
 public class Preview extends Activity implements SurfaceHolder.Callback{
 	Camera camera;
@@ -53,9 +55,11 @@ public class Preview extends Activity implements SurfaceHolder.Callback{
 							    cameraCount = Camera.getNumberOfCameras();
 							    for ( int camIdx = 0; camIdx < cameraCount; camIdx++ ) {
 							        Camera.getCameraInfo( camIdx, cameraInfo );
+							        
 							        if ( cameraInfo.facing == Camera.CameraInfo.CAMERA_FACING_BACK  ) {
 							            try {
 							                camera = Camera.open( camIdx );
+							                setCameraDisplayOrientation(Preview.this,camIdx,camera);
 							            } catch (RuntimeException e) {
 							                Log.e("TAG", "Camera failed to open: " + e.getLocalizedMessage());
 							            }
@@ -101,6 +105,7 @@ public class Preview extends Activity implements SurfaceHolder.Callback{
 								        if ( cameraInfo.facing == Camera.CameraInfo.CAMERA_FACING_FRONT  ) {
 								            try {
 								                camera = Camera.open( camIdx );
+								                setCameraDisplayOrientation(Preview.this,camIdx,camera);
 								            } catch (RuntimeException e) {
 								                Log.e("TAG", "Camera failed to open: " + e.getLocalizedMessage());
 								            }
@@ -145,6 +150,28 @@ public class Preview extends Activity implements SurfaceHolder.Callback{
 			
 
 		}
+		public static void setCameraDisplayOrientation(Activity activity,
+	         int cameraId, android.hardware.Camera camera) {
+	   android.hardware.Camera.CameraInfo info = new android.hardware.Camera.CameraInfo();
+	   android.hardware.Camera.getCameraInfo(cameraId, info);
+	   int rotation = activity.getWindowManager().getDefaultDisplay().getRotation();
+	   int degrees = 0;
+	   switch (rotation) {
+	         case Surface.ROTATION_0: degrees = 0; break;
+	         case Surface.ROTATION_90: degrees = 90; break;
+	         case Surface.ROTATION_180: degrees = 180; break;
+	         case Surface.ROTATION_270: degrees = 270; break;
+	   }
+
+	   int result;
+	   if (info.facing == Camera.CameraInfo.CAMERA_FACING_FRONT) {
+	       result = (info.orientation + degrees) % 360;
+	       result = (360 - result) % 360;  // compensate the mirror
+	   } else {  // back-facing
+	       result = (info.orientation - degrees + 360) % 360;
+	   }
+	   camera.setDisplayOrientation(result);
+	}
 
 	@Override
 	public void surfaceChanged(SurfaceHolder holder, int format, int width, int height)
