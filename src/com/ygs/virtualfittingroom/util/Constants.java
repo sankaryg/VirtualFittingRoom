dae9d7f114da1555a3f4a2a38546e66cb538a799
sankar <ygsankar.87@gmail.com>
2014-03-29 16:03:44 +0530

    Update Constants.java

--- src/com/ygs/virtualfittingroom/util/Constants.java
+++ src/com/ygs/virtualfittingroom/util/Constants.java
@@ -1,7 +1,6 @@
 package com.ygs.virtualfittingroom.util;
 
 public class Constants {
-	public static final String ACCESS_KEY_ID = "AKIAIRFKDWSHUNI6M55A";
-	public static final String SECRET_KEY = "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";
+	
 	
 }
